c65bc103472e9db38b3d06090c887d4306b560d6
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2024-04-15 07:59:52 -0300

    first commit

---
+++ src/main/java/com/ferraz/controledepagamentosbackend/ControleDePagamentosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ferraz.controledepagamentosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDePagamentosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDePagamentosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferraz/controledepagamentosbackend/ControleDePagamentosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferraz.controledepagamentosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDePagamentosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
