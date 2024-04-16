24c5939f67a7cda076d2ac19e65394906c1bef07
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2024-04-16 08:08:04 -0300

    teste protecao branch

--- src/main/java/com/ferraz/controledepagamentosbackend/ControleDePagamentosBackendApplication.java
+++ src/main/java/com/ferraz/controledepagamentosbackend/ControleDePagamentosBackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ControleDePagamentosBackendApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ControleDePagamentosBackendApplication.class, args);
 	}
