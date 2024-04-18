31d2e3cd4ceb1e5be023c085c3fe964406372af6
BBQueiroz <bruninhoqueiroz@gmail.com>
2024-04-17 21:56:28 -0300

    parametro_repository

---
+++ src/main/java/com/ferraz/controledepagamentosbackend/domain/parameters/ParametroRepository.java
@@ -0,0 +1,7 @@
+package com.ferraz.controledepagamentosbackend.domain.parameters;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParametroRepository extends JpaRepository<Parametro, Long> {
+
+}
