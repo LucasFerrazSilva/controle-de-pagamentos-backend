e6f5f5701330e6787948d336a417e5a22e5bd52a
BBQueiroz <bruninhoqueiroz@gmail.com>
2024-04-20 02:16:19 -0300

    Update ParametroController.java
    Adicionado endpoints para get, put e delete

--- src/main/java/com/ferraz/controledepagamentosbackend/controller/ParametroController.java
+++ src/main/java/com/ferraz/controledepagamentosbackend/controller/ParametroController.java
@@ -3,12 +3,15 @@ package com.ferraz.controledepagamentosbackend.controller;
 import com.ferraz.controledepagamentosbackend.domain.parameters.Parametro;
 import com.ferraz.controledepagamentosbackend.domain.parameters.ParametroService;
 import com.ferraz.controledepagamentosbackend.domain.parameters.dto.NovoParametroDTO;
+import com.ferraz.controledepagamentosbackend.domain.parameters.dto.UpdateParametroDTO;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/parametros")
@@ -23,9 +26,40 @@ public class ParametroController {
 
     @PostMapping
     public ResponseEntity<Object> createParameter(@RequestBody @Valid NovoParametroDTO novoParametroDTO){
-        Parametro novoParemetro = parametroService.save(novoParametroDTO);
-        return ResponseEntity.ok(novoParemetro);
+        Parametro novoParametro = parametroService.save(novoParametroDTO);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(novoParametro.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(novoParametroDTO);
     }
 
+    @GetMapping
+    public ResponseEntity<List<Parametro>> getAllParameters(){
+        List<Parametro> parametros = parametroService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(parametros);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Parametro> getOneParameter(@PathVariable(value = "id") Long id){
+        Parametro parametro = parametroService.findOne(id);
+        return ResponseEntity.status(HttpStatus.OK).body(parametro);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateParameter(@PathVariable(value = "id") Long id, @RequestBody @Valid UpdateParametroDTO updateParametroDTO){
+        parametroService.updateParametro(id, updateParametroDTO);
+
+        return ResponseEntity.status(HttpStatus.OK).body(updateParametroDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deactivateParameter(@PathVariable(value = "id") Long id){
+        parametroService.deactivateParametro(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 
 }
