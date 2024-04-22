1430894ec5b05e597a583fed244e99a3b9b134d3
BBQueiroz <bruninhoqueiroz@gmail.com>
2024-04-21 21:36:00 -0300

    Update UpdateParametroDTO.java

--- src/main/java/com/ferraz/controledepagamentosbackend/domain/parameters/dto/UpdateParametroDTO.java
+++ src/main/java/com/ferraz/controledepagamentosbackend/domain/parameters/dto/UpdateParametroDTO.java
@@ -3,6 +3,7 @@ package com.ferraz.controledepagamentosbackend.domain.parameters.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record UpdateParametroDTO(
+        Long id,
         @NotBlank
         String nome,
         @NotBlank
