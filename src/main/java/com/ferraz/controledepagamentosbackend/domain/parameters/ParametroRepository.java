f48a81c5d5045289e1ae151e08bb076ea577661d
BBQueiroz <bruninhoqueiroz@gmail.com>
2024-04-25 01:00:16 -0300

    Removed unused imports

--- src/main/java/com/ferraz/controledepagamentosbackend/domain/parameters/ParametroRepository.java
+++ src/main/java/com/ferraz/controledepagamentosbackend/domain/parameters/ParametroRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface ParametroRepository extends JpaRepository<Parametro, Long> {
     Page<Parametro> findAllByStatus(Pageable pageable, ParametroStatus parametroStatus);
 
