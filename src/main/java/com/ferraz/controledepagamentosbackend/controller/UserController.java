9e7759f7d7bcf409daeb7cbe41beff09b201fab4
Luis Vanique <cunhagustavo142@gmail.com>
2024-04-24 23:59:00 -0300

    adiciona-filtros-requisicao-listar-user

--- src/main/java/com/ferraz/controledepagamentosbackend/controller/UserController.java
+++ src/main/java/com/ferraz/controledepagamentosbackend/controller/UserController.java
@@ -3,6 +3,9 @@ package com.ferraz.controledepagamentosbackend.controller;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +14,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.ferraz.controledepagamentosbackend.domain.user.User;
 import com.ferraz.controledepagamentosbackend.domain.user.UserService;
+import com.ferraz.controledepagamentosbackend.domain.user.UserStatus;
 import com.ferraz.controledepagamentosbackend.domain.user.dto.DadosAtualizacaoUserDTO;
 import com.ferraz.controledepagamentosbackend.domain.user.dto.DadosCreateUserDTO;
 import com.ferraz.controledepagamentosbackend.domain.user.dto.UserDTO;
@@ -41,8 +46,12 @@ public class UserController {
 	}
 	
 	@GetMapping
-	public ResponseEntity<List<UserDTO>> listar() {
-		return ResponseEntity.ok().body(userService.listarUsuarios());
+	public ResponseEntity<Page<UserDTO>> listar(@PageableDefault Pageable pageable, 
+			@RequestParam(required = false) String nome, @RequestParam(required = false) String email, 
+			@RequestParam(required = false) String perfil,@RequestParam(required = false) UserStatus status) {
+		Page<User> users = userService.listarUsuarios(pageable, nome, email, perfil,status);
+		Page<UserDTO> pageDTO = users.map(UserDTO::new);
+		return ResponseEntity.ok().body(pageDTO);
 	}
 	
 	@DeleteMapping("/{id}")
