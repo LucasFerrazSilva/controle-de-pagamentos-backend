5e60003e8cda33b6d67fe331c35a4d5232667c3b
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2024-05-06 20:51:16 -0300

    adiciona configuracoes para envio de email

---
+++ src/main/java/com/ferraz/controledepagamentosbackend/infra/email/EmailService.java
@@ -0,0 +1,39 @@
+package com.ferraz.controledepagamentosbackend.infra.email;
+
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.springframework.mail.javamail.MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED;
+
+@Service
+public class EmailService {
+
+    private JavaMailSender mailSender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public Boolean sendMailSMTP(EmailDTO emailDTO) {
+        try{
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, MULTIPART_MODE_MIXED_RELATED, UTF_8.name());
+            helper.setSubject(emailDTO.subject());
+            helper.setFrom(emailDTO.from());
+            helper.setTo(emailDTO.to());
+            helper.setText(emailDTO.html(), true);
+            mailSender.send(message);
+            return true;
+        }catch (Exception ex) {
+            Logger.getLogger(EmailService.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+}
