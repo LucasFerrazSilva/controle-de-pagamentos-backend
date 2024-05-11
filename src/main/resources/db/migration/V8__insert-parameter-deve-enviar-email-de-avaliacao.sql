3b2108339718c78b25b1b36f78a8957bdf700026
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2024-05-10 21:56:03 -0300

    refactor: desliga parametro de envio de e-mails

--- src/main/resources/db/migration/V8__insert-parameter-deve-enviar-email-de-avaliacao.sql
+++ src/main/resources/db/migration/V8__insert-parameter-deve-enviar-email-de-avaliacao.sql
@@ -1,2 +1,2 @@
-INSERT INTO TB_PARAMETERS VALUES (2, 'Deve enviar e-mail de solicitação de avaliação de horas extras? (S/N)', 'S', 'ATIVO', NOW(), NULL, NULL, NULL);
+INSERT INTO TB_PARAMETERS VALUES (2, 'Deve enviar e-mail de solicitação de avaliação de horas extras? (S/N)', 'N', 'ATIVO', NOW(), NULL, NULL, NULL);
 COMMIT;
