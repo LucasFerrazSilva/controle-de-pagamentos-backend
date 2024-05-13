8d7cd83cd4151abe615794cc8670777cc97b3240
Luis Vanique <cunhagustavo142@gmail.com>
2024-05-12 22:19:41 -0300

    resolve issues sonarquce

--- src/main/java/com/ferraz/controledepagamentosbackend/domain/horasextras/HorasExtras.java
+++ src/main/java/com/ferraz/controledepagamentosbackend/domain/horasextras/HorasExtras.java
@@ -1,16 +1,27 @@
 package com.ferraz.controledepagamentosbackend.domain.horasextras;
 
+import java.time.LocalDateTime;
+
 import com.ferraz.controledepagamentosbackend.domain.horasextras.dto.AtualizarHorasExtrasDTO;
-import com.ferraz.controledepagamentosbackend.domain.horasextras.dto.AvaliarHorasDTO;
 import com.ferraz.controledepagamentosbackend.domain.horasextras.dto.NovasHorasExtrasDTO;
 import com.ferraz.controledepagamentosbackend.domain.user.User;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "TB_HORAS_EXTRAS")
