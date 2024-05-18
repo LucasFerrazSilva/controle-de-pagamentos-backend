c50c48c68e7f10c40c47eea230fd2fc74dd5785e
Bruno Queiroz <120342016+BBQueiroz@users.noreply.github.com>
2024-05-18 02:50:00 -0300

    Update UserController.java

--- src/main/java/com/ferraz/controledepagamentosbackend/controller/UserController.java
+++ src/main/java/com/ferraz/controledepagamentosbackend/controller/UserController.java
@@ -9,7 +9,6 @@ import com.ferraz.controledepagamentosbackend.domain.user.dto.DadosCreateUserDTO
 import com.ferraz.controledepagamentosbackend.domain.user.dto.NovaSenhaDTO;
 import com.ferraz.controledepagamentosbackend.domain.user.dto.UserDTO;
 import jakarta.validation.Valid;
-import org.apache.coyote.BadRequestException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
