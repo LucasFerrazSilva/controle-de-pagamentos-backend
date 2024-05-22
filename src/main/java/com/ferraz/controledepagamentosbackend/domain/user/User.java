38e0676f706cf823c5f4e095b3b342ca619e5b87
Luis Vanique <cunhagustavo142@gmail.com>
2024-05-21 22:43:29 -0300

    resolve issue

--- src/main/java/com/ferraz/controledepagamentosbackend/domain/user/User.java
+++ src/main/java/com/ferraz/controledepagamentosbackend/domain/user/User.java
@@ -5,8 +5,6 @@ import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
-import com.ferraz.controledepagamentosbackend.domain.parameters.ParametroStatus;
-import com.ferraz.controledepagamentosbackend.domain.user.dto.NovaSenhaDTO;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
